6bd3f7e21c25ac1ec5ccc5b8bd27889247f262f6
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2021-09-24 19:57:23 +0700

    thêm class Cow+TextDrive

---
+++ src/vn/edu/giadinh/tuan03/Cow.java
@@ -0,0 +1,19 @@
+/*
+*   created date: Sep 20,2021
+*    author: HaiAnh
+*
+*/
+package vn.edu.giadinh.tuan03;
+
+public class Cow {
+    double weight;
+    
+    //HÀM TẠO
+    //developers tạo ra không để java compiler tạo ra
+    Cow(){//HÀM TẠO MẶC ĐỊNH DO developer tạo ra
+
+    }
+    Cow(double w){
+       weight=w;
+    }
+}
